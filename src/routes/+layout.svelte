9ed38578c4dd5f619091c1ef2a0c4c42dcea937b
Harenc <hare0319@gmail.com>
2023-01-28 22:55:46 +0800

    feat: :sparkles: Added a dropdown button component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<div class="container">
+  <slot />
+</div>
+
+<style>
+  .container {
+    width: 80%;
+    height: 100%;
+    margin: auto;
+  }
+</style>
